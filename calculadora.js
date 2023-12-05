26d936e0a2ebeea6d061e595922e36b552fd60b6
sofiia vargas <130010214+SOFII38A@users.noreply.github.com>
2023-12-05 09:36:22 -0300

    commit calculadora

---
+++ calculadora.js
@@ -0,0 +1,23 @@
+function limpiar() {
+    document.getElementById('miFormulario').requestFullscreen();
+}
+function sumar(){
+    var x = parseInt(document.getElementById('valor1').value);
+    var y = parseInt(document.getElementById('valor2').value);
+    document.getElementById('Resultado').innerHTML = x+y;
+}
+function restar(){
+    var x = parseInt(document.getElementById('valor1').value);
+    var y = parseInt(document.getElementById('valor2').value);
+    document.getElementById('Resultado').innerHTML = x-y;
+}
+function multiplicar(){
+    var x = parseInt(document.getElementById('valor1').value);
+    var y = parseInt(document.getElementById('valor2').value);
+    document.getElementById('Resultado').innerHTML = x*y;
+}
+function dividir(){
+    var x = parseInt(document.getElementById('valor1').value);
+    var y = parseInt(document.getElementById('valor2').value);
+    document.getElementById('Resultado').innerHTML = x/y;
+}
